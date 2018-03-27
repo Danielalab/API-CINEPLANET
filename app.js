30bba4c6ae18218562a42d04a103532bbcd3d98b
Daniela Gonzales <olabolacrayola@hotmail.com>
2018-03-27 16:46:38 -0500

    modifica use app

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
 const app = express()
 const api = require('./routes')
-
+app.use(function (req, res ,next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next()
+})
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.engine('.hbs', hbs({
@@ -18,13 +24,7 @@ app.use('/api', api)
 app.use('/login', (req, res) => {
     res.render('login')
 })
-app.use(function (req, res ,next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next()
-})
+
 app.get('/', (req, res) => {
     res.render('product')
 })
